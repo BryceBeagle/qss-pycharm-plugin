c0febf4af3431ffc4babcd6b18657865b0f05fce
ignormies <bryce.beagle@gmail.com>
2020-03-22 01:12:20 -0700

    Initial repo structure

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("org.jetbrains.intellij") version "0.4.16"
+    java
+    kotlin("jvm") version "1.3.70"
+}
+
+group = "com.ignormies.qss"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testCompile("junit", "junit", "4.12")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    pluginName = "QSS"
+    version = "LATEST-EAP-SNAPSHOT"
+    type = "PY"
+}
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "qss-pycharm-plugin"

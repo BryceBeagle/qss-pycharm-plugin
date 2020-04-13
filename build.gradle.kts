57f17cc2384b0b7e0090e254d2406c62831b4c4f
ignormies <bryce.beagle@gmail.com>
2020-04-13 00:57:13 -0700

    Build parser and lexer as dependencies for plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import org.jetbrains.grammarkit.tasks.GenerateLexer
+import org.jetbrains.grammarkit.tasks.GenerateParser
+
 plugins {
-    id("org.jetbrains.intellij") version "0.4.16"
+    id("org.jetbrains.intellij") version "0.4.18"
+    id("org.jetbrains.grammarkit") version "2020.1"
     java
     kotlin("jvm") version "1.3.70"
 }
@@ -11,12 +15,8 @@ repositories {
     mavenCentral()
 }
 
-// Not sure if this is the best way to do this. Lets the generated code get used
-sourceSets["main"].java.srcDir("src/main/gen")
-
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    testCompile("junit", "junit", "4.12")
 }
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
@@ -28,17 +28,46 @@ intellij {
         "PsiViewer:201.6251.22-EAP-SNAPSHOT.3"
     )
 }
+
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
-tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+
+sourceSets {
+    main {
+        java.srcDir("src/main/gen")
     }
-    compileTestKotlin {
+}
+
+tasks {
+
+    val generateQSSParser by registering(GenerateParser::class) {
+        source = "src/main/kotlin/ignormies/qss/QSS.bnf"
+        targetRoot = "src/main/gen/"
+        pathToParser = "ignormies/qss/parser/QSSParser.java"
+        pathToPsiRoot = "ignormies/qss/psi/"
+        purgeOldFiles = true
+    }
+
+    val generateQSSLexer by registering(GenerateLexer::class) {
+        source = "src/main/kotlin/ignormies/qss/QSS.flex"
+        targetDir = "src/main/gen/ignormies/qss/"
+        targetClass = "QSSLexer"
+        purgeOldFiles = true
+    }
+
+    compileJava {
+        dependsOn(generateQSSParser)
+        dependsOn(generateQSSLexer)
+    }
+
+    compileKotlin {
+        dependsOn(generateQSSParser)
+        dependsOn(generateQSSLexer)
         kotlinOptions.jvmTarget = "1.8"
     }
 }
+
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
     changeNotes("""Initial release""")
 }
